da57b9a27139826863a26638940a3a87186ed369
wally_yoon <bluegmlduf2@naver.com>
2025-02-11 14:10:21 +0900

    add: first commit

---
+++ lib/data/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color.fromARGB(255, 58, 149, 255);
+Color reallyLightGrey = Colors.grey.withAlpha(25);
+ThemeData appThemeLight =
+    ThemeData.light().copyWith(primaryColor: primaryColor);
